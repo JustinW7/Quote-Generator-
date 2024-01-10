c237bc8cc5a0c72b6105b0aec79c43575ba726b5
Justin <justinvarghesemicheal@gmail.com>
2024-01-10 16:20:00 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+import React from 'react';
+import QuoteGenerator from './QuoteGenerator';
+
+
+function App() {
+  return (
+    <div className="App">
+
+
+
+
+      <h1>Random Quotes Generator</h1>
+      <QuoteGenerator />
+    </div>
+  );
+}
+
+export default App;
+
